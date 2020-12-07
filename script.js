21d30fb8a625cf06d723aac5e25f99c0f981e368
Lois Simonca <loisgabor@gmail.com>
2020-12-07 12:47:41 -0500

    new styling, new portfolio project

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+    $(document).ready(function () {
+        $("nav a").click(function () {
+            $("nav a").removeClass("active");
+            $(this).addClass("active");
+        });
+    });
+</script>
