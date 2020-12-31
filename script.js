b142176354dd5b9e1bddfb69daad7344113665a4
Lois Simonca <loisgabor@gmail.com>
2020-12-30 19:55:24 -0500

    updated look and inmproved content

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-<script>
-    $(document).ready(function () {
-        $("nav a").click(function () {
-            $("nav a").removeClass("active");
-            $(this).addClass("active");
-        });
-    });
-</script>
+console.log("hello");
+const main = document.querySelector(".main");
+const nav = document.querySelector(".navbar");
+
+const offset = main.offsetHeight - nav.offsetHeight;
+
+window.onscroll = function () {
+  if (window.pageYOffset > offset) {
+    nav.classList.remove("bottom-nav");
+    nav.classList.add("top-nav");
+  } else {
+    nav.classList.add("bottom-nav");
+    nav.classList.remove("top-nav");
+  }
+};
